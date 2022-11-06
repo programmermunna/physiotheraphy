6441ced6e4817a6d40a1a955dba26788bc5d2c13
munna <freelancermunna4@gmail.com>
2022-11-06 11:33:38 +0600

    somethig update

--- common/footer.php
+++ common/footer.php
@@ -6,29 +6,29 @@
           <ul class="footer_right">
             
             <li>
-              <a href="tel:<?php echo $website['phone'];?>" style="color: #308CFE">
+              <a href="tel:<?php echo $website['phone'];?>" style="color: #308CFE" target="_blank">
               <i class="fa-solid fa-square-phone"></i>
               </a>
             </li>
             <li>
-              <a href="mailto:<?php echo $website['gmail'];?>" style="color: #308CFE">
+              <a href="mailto:<?php echo $website['gmail'];?>" style="color: #308CFE" target="_blank">
               <i class="fa-solid fa-square-envelope"></i>
               </a>
             </li>
             <li>
-              <a href="<?php echo $website['facebook'];?>" style="color: #308CFE">
+              <a href="<?php echo $website['facebook'];?>" style="color: #308CFE" target="_blank">
               <i class="fa-brands fa-facebook"></i>
               </a>
             </li>
 
             <li>
-              <a href="<?php echo $website['youtube'];?>" style="color: #308CFE">
+              <a href="<?php echo $website['youtube'];?>" style="color: #308CFE" target="_blank">
               <i class="fa-brands fa-youtube"></i>
               </a>
             </li>
             
             <li>
-              <a href="<?php echo $website['linkedin'];?>" style="color: #308CFE">
+              <a href="<?php echo $website['linkedin'];?>" style="color: #308CFE" target="_blank">
                 <i class="fab fa-linkedin"></i>
               </a>
             </li>
@@ -36,12 +36,7 @@
         </div>
       </footer>
 
-
-
       <div class="chatbox"><?php echo $website['chatbox'];?></div>
-
-
-
       
   <script src="js/jquery.min.js"></script>
   <script src="lib/slicknav/slicknav.js"></script>
